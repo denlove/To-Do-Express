cad52487c8940a7a701b007c8e86aa41071f6d91
denlove <bronzovdenis59@gmail.com>
2024-04-04 16:05:24 +0300

    TDE-1: edit index.ts according to prettier style

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app: Express = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, World! It\'s me :D')
+    res.send("Hello, World! It's me :D")
 })
 
 app.listen(port, () => {
