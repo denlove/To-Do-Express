2f94c09ef3ec254f35751ce2e80fb139d6040af1
denlove <bronzovdenis59@gmail.com>
2024-04-08 19:37:55 +0300

    TDE-3: add new mock logs and routes

--- src/utils/mock.ts
+++ src/utils/mock.ts
@@ -1,4 +1,8 @@
 export const SERVER_RUNNING_LOG = "Server :: Running @ 'http://localhost:"
+export const MOUNTING_API_LOG = 'Routes :: Mounting API Routes...'
+export const MOUNTING_PATH_LOG = 'Routes :: Mounting Path Routes...'
+export const BOOTING_HTTP_LOG = "Booting the 'HTTP' middleware ...'"
+
 export const ERROR_CLOSING_LOG = 'Error closing log file that was being appended'
 export const ERROR_APPENDING_LOG = 'Error appending to the log file'
 export const ERROR_OPENING_LOG = "Error cloudn't open the log file for appending"
