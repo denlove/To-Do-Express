037ad02b3fd22905ff89b9877c6e05124fbc66ad
denlove <bronzovdenis59@gmail.com>
2024-04-11 17:53:41 +0300

    TDE-4: minor fixes for tests

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 
 class HomeController {
     public static homePage(req: Request, res: Response) {
+        res.set({ 'Content-Type': 'text/plain', 'Confirm-Test': 'confirm' })
         res.status(200).send('Hello, World!')
     }
     public static getRandomWelcomeWord() {}
